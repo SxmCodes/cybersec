ed77848b433ad683cdb7813b26995551f934e16c
Saksham Sharma <sakshamsharma1029@gmail.com>
2022-02-06 17:45:30 +0530

    updated pattern file.

---
+++ program/python/ques/pattern.py
@@ -0,0 +1,10 @@
+# This will call for 5 and this outer loop is for rows.
+for i in range(6):
+    # This loop is for coloumns.
+    for j in range(i):
+        # end=' ' will print a  space after the displayed string instead of a newline.
+        print(i , end=' ')
+
+    # This will print new line after each row.
+    print(' ')
+
